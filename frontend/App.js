bea71629a4874afb9df8eb71739e3de3cf3e0203
Mohammad Kumail <40884947+kumail11@users.noreply.github.com>
2022-08-18 16:54:11 +0500

    Add files via upload

---
+++ frontend/App.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import Axios from 'axios';
+
+function App() {
+
+  const [movieName, setMovieName] = useState("");
+  const [review, setReview] = useState("");
+  const [movieReviewList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/api/get").then((response) => {
+      setMovieList(response.data);
+    });
+  }, []);
+
+  const submitReview = () => {
+    Axios.post("http://localhost:3001/api/insert", {
+      movieName: movieName,
+      movieReview: review,
+    }).then(() => {
+      alert("Successfully Inserted..");
+    });
+  };
+
+  return (
+    <div className="App">
+      <h1>CRUD APPLICATION</h1>
+      
+      <div className="form">
+        <label>Movie Name:</label>
+        <input 
+          type="text"
+          name="movieName"
+          onChange={(e) => {
+            setMovieName(e.target.value);
+          }}
+          />
+        
+        <label>Review:</label>
+        <input 
+          type="text" 
+          name="review"
+          onChange={(e) => {
+            setReview(e.target.value);
+          }}
+          />
+
+        <button onClick={submitReview}>Submit</button>
+
+        {movieReviewList.map((val) => {
+          return <h1>MovieName: {val.movieName} | MovieReview: {val.movieReview}</h1>
+        })}
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
